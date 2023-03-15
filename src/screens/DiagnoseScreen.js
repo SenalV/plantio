3f6aca56f7c1c64e506d4437b1d65011c54d5f50
SenalVithana <senalpulindu@gmail.com>
2023-03-15 15:56:12 +0530

    camera and image picker setup(expo)

--- src/screens/DiagnoseScreen.js
+++ src/screens/DiagnoseScreen.js
@@ -1,10 +1,115 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { Camera, CameraType } from "expo-camera";
 
 export default function DiagnoseScreen() {
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <View className="h-screen p-5 mt-8">
-      <Text className="text-xl font-semibold">Diagnose the disease.</Text>
-      <Text className="text-gray-500">Capture an image to diagnose and for remedies. </Text>
-    </View>
+    <ScrollView>
+      <View className="h-screen p-5 mt-8">
+        <Text className="text-xl font-semibold">Diagnose the disease.</Text>
+        <Text className="text-gray-500">
+          Diagnose the disease and find out what you can do to solve.
+        </Text>
+
+        <View className="p-5 rounded-xl flex items-center justify-center mt-10 bg-slate-300">
+          <Text className=" text-gray-500 font-bold text-base">
+            Upload image to diagnose.
+          </Text>
+          <Text className="mb-3 text-green-700 text-sm">
+            Select and upload an image to diagnose the disease.
+          </Text>
+          <TouchableOpacity onPress={pickImage}>
+            {/* <Button title="Upload Image" /> */}
+            <View className="bg-green-700 p-3 w-2/5 flex items-center justify-center rounded-xl">
+              <Text className="font-bold text-sm text-green-100">
+                Upload Image
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        {image && (
+          <View className="flex justify-center w-full items-center mt-10">
+            <Text className="mb-3 text-gray-500 text-center w-4/5">
+              Uploaded Image, upload again to change or click Diagnose to
+              proceed
+            </Text>
+            <Image
+              source={{ uri: image }}
+              style={{ width: 200, height: 200 }}
+              className="rounded-lg"
+            />
+            <TouchableOpacity className="mt-5">
+              <View className="bg-slate-600 p-3 w-2/5 flex items-center justify-center rounded-xl">
+                <Text className="font-bold text-sm text-blue-100">
+                  Diagnose
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        )}
+
+        <View className="p-5 rounded-xl flex items-center justify-center mt-10 bg-slate-300">
+          <Text className=" text-gray-500 font-bold text-base">
+            Capture image to diagnose.
+          </Text>
+          <Text className="mb-3 text-green-700 text-sm text-center">
+            Capture an image using camera to diagnose the disease.
+          </Text>
+          <TouchableOpacity onPress={pickImage}>
+            <View className="bg-blue-600 p-3 w-2/5 flex items-center justify-center rounded-xl">
+              <Text className="font-bold text-sm text-blue-100">
+                Capture Image
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      {/* <View>
+        <Camera type={type} className="h-full mb-10">
+          <View>
+            <TouchableOpacity onPress={toggleCameraType} className>
+              <Text>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View> */}
+
+    </ScrollView>
   );
 }
+
+const Button = (title) => {
+  return (
+    <View className="bg-green-700 p-3 w-2/5 flex items-center justify-center rounded-xl">
+      <Text className="font-bold text-sm text-green-100">Upload Image</Text>
+    </View>
+  );
+};
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,43 @@
 import { View, Text } from "react-native";
 import { Icon } from "react-native-eva-icons";
 import { TouchableOpacity } from "react-native";
+import { useEffect } from "react";
+import axios from "axios";
 
 export default function HomeScreen() {
+  const url = "http://127.0.0.1:8000/ping";
+
+  const getPing = () => {
+    axios
+      .get("http://127.0.0.1:8000/ping")
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    // fetch("http://127.0.0.1:8000/ping")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     console.log(json);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+  };
+
+  useEffect(() => {
+    getPing();
+  }, []);
+
   return (
     <View className="h-screen p-5 mt-8">
       <View className="flex flex-row items-center justify-between">
         <View>
           <Text className="text-xl font-semibold">
             Welcome Back,
-            <Text className="text-green-800"> John</Text>
+            <Text className="text-green-800"> Johnss</Text>
           </Text>
           <Text className="text-gray-500">
             Start treating your plants with Plantio.
