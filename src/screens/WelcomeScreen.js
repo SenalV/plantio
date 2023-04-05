513ce56ea80dbba6e9e01e35c7884423d68dd1e7
iarfith <118873269+iarfith@users.noreply.github.com>
2023-04-06 02:16:15 +0530

    Login ,SignUp

---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,41 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { themeColors } from '../theme'
+// import { useNavigation } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
+
+export default function WelcomeScreen() {
+    const navigation = useNavigation();
+  return (
+    <SafeAreaView className="flex-1" style={{backgroundColor: '#FFFFFF'}}>
+        <View className="flex-1 flex justify-around my-4">
+            <Text 
+                className="text-black font-bold text-4xl text-center">
+                Welcome to Plantio
+            </Text>
+            <View className="flex-row justify-center">
+                <Image source={require("../assets/images/plant6.png")}
+                    style={{width: 300, height: 520}} />
+            </View>
+            <View className="space-y-4">
+                <TouchableOpacity
+                    onPress={()=> navigation.navigate('SignUp')}
+                    className="py-3  mx-7 rounded-xl" style={{backgroundColor: '#569033'}}>
+                        <Text 
+                            className="text-xl font-bold text-center text-gray-700 "
+                        >
+                            Sign Up
+                        </Text>
+                </TouchableOpacity>
+                <View className="flex-row justify-center">
+                    <Text className="text-black font-semibold">Already have an account?</Text>
+                    <TouchableOpacity onPress={()=> navigation.navigate('Login')}>
+                        <Text className="font-semibold text-green-600"> Log In</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    </SafeAreaView>
+  )
+}
